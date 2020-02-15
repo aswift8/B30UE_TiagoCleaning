6d08179ad32b506aff5d50f590eb00841e466158
DigitalTether <niko.tether@sisu-fm.co.uk>
2020-02-15 00:38:45 +0000

    Path control script plus simulator script (first attempt). Code runs without crashing so far, but doesn't yet change over to the next path section properly.

---
+++ path_trajectory_pack/src/EndEffectorSim.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Vector3
+
+
+class Memory:
+    current_position = Vector3(0, 0, 0)
+    current_rotation = Float32MultiArray()
+    current_velocity = Vector3(0, 0, 0)
+    receive_velocity_from = "/path/desired_velocity"
+    give_position_to = "/path/current_position"
+    give_rotation_to = "/path/current_rotation"
+    rate = 10
+
+
+def update_velocity(velocity_data):
+    Memory.current_velocity = velocity_data
+
+
+def update_pose():
+    Memory.current_position.x += (Memory.current_velocity.x / Memory.rate)
+    Memory.current_position.y += (Memory.current_velocity.y / Memory.rate)
+    Memory.current_position.z += (Memory.current_velocity.z / Memory.rate)
+    Memory.current_velocity = Vector3(0, 0, 0)
+
+
+def get_current_position():
+    return Memory.current_position
+
+
+def get_current_rotation():
+    return Memory.current_rotation
+
+
+Memory.current_rotation.data = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+rospy.init_node("end_effector_sim")
+rate = rospy.Rate(Memory.rate)
+pub_pos = rospy.Publisher(Memory.give_position_to, Vector3, queue_size=5)
+pub_rot = rospy.Publisher(Memory.give_rotation_to, Float32MultiArray, queue_size=5)
+rospy.Subscriber(Memory.receive_velocity_from, Vector3, update_velocity)
+
+# wait for one second
+for i in range(0, Memory.rate):
+    rate.sleep()
+
+while not rospy.is_shutdown():
+    update_pose()
+    pub_pos.publish(get_current_position())
+    pub_rot.publish(get_current_rotation())
+    rate.sleep()
--- path_trajectory_pack/src/PathControl.py
+++ path_trajectory_pack/src/PathControl.py
@@ -4,53 +4,94 @@ import rospy
 import roslaunch
 import xml.etree.cElementTree
 import math
-from std_msgs.msg import String
-from geometry_msgs.msg import Vector3
+from std_msgs.msg import String, Float32MultiArray
+from geometry_msgs.msg import Vector3, Point
 
 
-# holds the current instruction before being processed
-class CurrentInstruction:
+class Memory:
     instruction = ""
+    correction_threshold = 0.01
+    z_abs_boundary_min = 0
+    z_abs_boundary_max = 2
+    threshold = 0.1
+    current_point_3d = Point(0, 0, 0)
+    current_rotation_3d = Float32MultiArray()
+    current_point_2d = (0, 0)
 
 
-# TODO define path reading method
-def readPath(filename):
+def distance_between_points(point_a, point_b):
+    delta_x_sq = math.pow(point_a.x - point_b.x, 2)
+    delta_y_sq = math.pow(point_a.y - point_b.y, 2)
+    delta_z_sq = math.pow(point_a.z - point_b.z, 2)
+    return math.sqrt(delta_x_sq + delta_y_sq + delta_z_sq)
+
+
+def update_position(data):
+    current_position = data
+    if distance_between_points(Memory.current_point_3d, current_position) > Memory.threshold:
+        Memory.current_point_3d = current_position
+        Memory.current_point_2d = transform_z(Memory.current_point_3d, False)
+
+
+def update_rotation(data):
+    Memory.current_rotation_3d = data
+
+
+def transform_z(vector, mode):
+    if mode:
+        vector_3d = Vector3()
+        vector_3d.x = vector[0]
+        vector_3d.y = vector[1]
+        vector_3d.z = 0
+        return vector_3d
+    else:
+        return vector.x, vector.y
+
+
+def read_path(filename):
     root = xml.etree.cElementTree.parse(filename).getroot()
     paths = []
     for point in root:
-        paths.append(tuple(point))
+        x = float(point.find("x").text)
+        y = float(point.find("y").text)
+        paths.append((x, y))
     return paths
 
 
-def calculateVelocity(start, end, speed):
+# TODO include error correction and rotational correction math
+def calculate_velocity(start_point, end_point, speed):
     vector = Vector3()
-    vector.z = 0
-    delta_x = end[0] - start[0]
-    if delta_x > 0:
-        vector.x = 1
-    elif delta_x < 0:
-        vector.x = -1
-    else:
-        vector.x = 0
-    delta_y = end[1] - start[1]
-    if delta_y > 0:
-        vector.y = 1
-    elif delta_y < 0:
-        vector.y = -1
-    else:
-        vector.y = 0
+    vector.x = end_point[0] - start_point[0]
+    vector.y = end_point[1] - start_point[1]
+
+    # error correction calculation
+    correction_magnitude = point_left_by(start_point, end_point, Memory.current_point_2d)
+    if abs(correction_magnitude) > Memory.correction_threshold:
+        correction_m = inverse_gradient(get_gradient(start_point, end_point))
+        if correction_m == "inf":
+            vector.y += correction_magnitude
+        else:
+            theta = math.atan(correction_m)
+            vector.x += correction_magnitude * math.cos(theta)
+            vector.y += correction_magnitude * math.sin(theta)
+
+    # z rotation correction
+    vector.z = -math.atan2(Memory.current_rotation_3d.data[1], Memory.current_rotation_3d.data[0])
+    if vector.z != 0:
+        new_x = vector.x * math.cos(vector.z) + vector.y * math.sin(vector.z)
+        new_y = vector.y * math.cos(vector.z) + vector.x * math.sin(vector.z)
+        vector.x = new_x
+        vector.y = new_y
+
     # normalises velocity
-    if vector.x != 0 and vector.y != 0:
+    if vector.x != 0 or vector.y != 0:
         magnitude = math.sqrt(math.pow(vector.x, 2) + math.pow(vector.y, 2))
         vector.x *= speed / magnitude
         vector.y *= speed / magnitude
-    else:
-        vector.x *= speed
-        vector.y *= speed
     return vector
 
 
-def getGradient(a, b):
+def get_gradient(a, b):
     delta_x = b[0] - a[0]
     if delta_x == 0:
         return "inf"
@@ -58,7 +99,7 @@ def getGradient(a, b):
         return (b[1] - a[1]) / delta_x
 
 
-def inverseGradient(m):
+def inverse_gradient(m):
     if m == "inf":
         return 0
     elif m == 0:
@@ -69,89 +110,107 @@ def inverseGradient(m):
 
 # where a and b are points on the line, and c is the point to check
 # if line is horizontal, returns true if the point is above line
-def pointIsLeft(a, b, c):
+def point_is_left(a, b, c):
     return ((b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])) >= 0
 
 
-def callback(data):
-    CurrentInstruction.instruction = data.data
+def point_left_by(a, b, c):
+    return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
+
+
+def new_instruction(data):
+    Memory.instruction = data.data
 
 
 rospy.init_node("path_controller")
 rospy_rate = 10
-rospy.Subscriber("/path/instruction", String, callback)
+rospy.Subscriber("/path/instruction", String, new_instruction)
+rospy.Subscriber("/path/current_position", Vector3, update_position)
+rospy.Subscriber("/path/current_rotation", Float32MultiArray, update_rotation)
 pub_desired_velocity = rospy.Publisher("/path/desired_velocity", Vector3, queue_size=5)
 pub_logger = rospy.Publisher("/path/logger", String, queue_size=5)
 rate = rospy.Rate(rospy_rate)
-calculate_velocity = False
+move_end_effector = False
 path_points = []
+Memory.current_rotation_3d.data = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+current_path_section = 0
+path_section_count = 0
+
+pub_logger.publish(String("path_controller started-up"))
 
 # if an xml file is specified, use that to create the first path upon start-up
 if len(roslaunch.sys.argv) > 1:
-    path_points = readPath(roslaunch.sys.argv[1])
+    path_points = read_path(roslaunch.sys.argv[1])
+    path_section_count = len(path_points) - 1
+    pub_logger.publish("new path imported: " + str(path_points))
 
-current_path_section = 0
-path_section_count = 0
-estimated_position = (0, 0)
 while not rospy.is_shutdown():
-    instruction = CurrentInstruction.instruction.split(" ")
-    if len(instruction) > 0:
-        if instruction[0] is "read":
-            path_points = readPath(instruction[1])
-            path_section_count = len(path_points) - 1
-            CurrentInstruction.instruction = ""
-            pub_logger.publish("new path imported")
-        elif instruction[0] is "start" and path_section_count > 0:
-            calculate_velocity = True
-            CurrentInstruction.instruction = ""
+    instruction = Memory.instruction.split(" ")
+    if len(instruction[0]) > 0: # if an instruction exists
+        pub_logger.publish("new instruction: " + str(instruction[0]))
+        if instruction[0] == "read":
+            if len(instruction) < 2: # read command must have a parameter
+                pub_logger.publish("read command requires a link to read from")
+                break
+            else:
+                path_points = read_path(instruction[1])
+                path_section_count = len(path_points) - 1
+                Memory.instruction = ""
+                move_end_effector = False
+                pub_logger.publish("new path imported: " + str(path_points))
+        elif instruction[0] == "start" and path_section_count > 0:
+            move_end_effector = True
+            Memory.instruction = ""
             pub_logger.publish("velocity calculation started")
-        elif instruction[0] is "stop":
-            calculate_velocity = False
-            CurrentInstruction.instruction = ""
+        elif instruction[0] == "stop":
+            move_end_effector = False
+            Memory.instruction = ""
             pub_logger.publish("velocity calculation ended")
-        elif instruction[0] is "reset":
-            calculate_velocity = False
+        elif instruction[0] == "reset":
+            move_end_effector = False
             current_path_section = 0
             path_section_count = 0
-            estimated_position = (0, 0)
-            CurrentInstruction.instruction = ""
+            Memory.instruction = ""
             pub_logger.publish("path_controller reset")
 
-    if calculate_velocity:
+    if move_end_effector:
         # identify current path section
         # stops moving end-effector if the end of the path has been reached
         plane_velocity = Vector3()
         section_identified = False
         while not section_identified:
-            if current_path_section >= path_section_count:
+            if current_path_section >= path_section_count: # if the end-effector has reached the end of its path
                 plane_velocity.x = 0
                 plane_velocity.y = 0
                 plane_velocity.z = 0
                 section_identified = True
+                rospy.loginfo("path end reached")
             else:
                 start = path_points[current_path_section]
                 end = path_points[current_path_section + 1]
-                crossover_line_gradient = inverseGradient(getGradient(start, end))
+                crossover_line_gradient = inverse_gradient(get_gradient(start, end))
+                rospy.loginfo(str(crossover_line_gradient))
                 end2_x = end[1] + 1
                 if crossover_line_gradient == "inf":
                     end2 = (end[0], end2_x)
+                elif crossover_line_gradient == 0:
+                    end2 = (end[0] + 1, end[1])
                 else:
                     c = end[1] / (crossover_line_gradient * end[0])
-                    end2 = ((crossover_line_gradient*end2_x) + c, end2_x)
-                start_crossover_bool = pointIsLeft(end, end2, start)
-                current_crossover_bool = pointIsLeft(end, end2, estimated_position)
+                    end2 = ((crossover_line_gradient * end2_x) + c, end2_x)
+
+                start_crossover_bool = point_is_left(end, end2, start)
+                current_crossover_bool = point_is_left(end, end2, Memory.current_point_2d)
+                rospy.loginfo(str(start_crossover_bool) + " " + str(current_crossover_bool))
                 if start_crossover_bool == current_path_section:
                     # we are on this current line
-                    plane_velocity = calculateVelocity(start, end, 0.1)
+                    plane_velocity = calculate_velocity(start, end, 0.5)
                     section_identified = True
                 else:
                     # we have moved to the next section
                     current_path_section += 1
         # publish this velocity
         pub_desired_velocity.publish(plane_velocity)
-        # update record of location TODO have a better localisation method
-        estimated_position[0] += plane_velocity.x / rospy_rate
-        estimated_position[1] += plane_velocity.y / rospy_rate
     rate.sleep()
 
 pub_logger.publish("path_controller shutting down")
